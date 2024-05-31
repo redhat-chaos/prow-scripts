b8c89c380fd474c71b57a059a64a7cb5c4401022
Paige Rubendall <prubenda@redhat.com>
2024-05-31 14:19:54 -0400

    set correct loc of scenario file
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED
    
    Signed-off-by: Paige Rubendall <prubenda@redhat.com>

--- zone-outages/prow_run.sh
+++ zone-outages/prow_run.sh
@@ -20,7 +20,7 @@ oc version
 envsubst < zone-outages/zone_outage_scenario.yaml.template > /tmp/zone_outage.yaml
 envsubst < config.yaml.template > /tmp/zone_config.yaml
 
-export SCENARIO_FILE=$krkn_loc/scenarios/zone_outage.yaml
+export SCENARIO_FILE=/tmp/zone_outage.yaml
 
 # Run Kraken
 cat /tmp/zone_outage.yaml
