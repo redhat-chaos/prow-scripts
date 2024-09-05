f58a97cf324c542fa91905a0308031d8ad980274
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2024-09-05 00:13:13 -0400

    Add junit xml parameters
    Tweak container and time scenarios to use them for Sippy to scrape them.
    
    Signed-off-by: Naga Ravi Chaitanya Elluri <nelluri@redhat.com>

--- container-scenarios/prow_run.sh
+++ container-scenarios/prow_run.sh
@@ -24,5 +24,14 @@ envsubst < config.yaml.template > /tmp/container_scenario_config.yaml
 # Run Kraken
 cat /tmp/container_scenario_config.yaml
 cat /tmp/container_scenario.yaml
-python3.9 $krkn_loc/run_kraken.py --config=/tmp/container_scenario_config.yaml -o /tmp/report.out
 
+[ -z $JUNIT_TESTCASE ] && JUNIT_TESTCASE="container-scenarios"
+[ -z $ARTIFACT_DIR ] && ARTIFACT_DIR="/tmp"
+
+TEST_VERSION=`oc version -o json | jq .openshiftVersion`
+
+python3.9 $krn_loc/run_kraken.py --config=/tmp/container_scenario_config.yaml \
+-o /tmp/report.out \
+--junit-testcase "$JUNIT_TESTCASE" \
+--junit-testcase-path "$ARTIFACT_DIR" \
+--junit-testcase-version "$TEST_VERSION"
