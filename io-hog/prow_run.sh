6256b22f9cee7c30f222ea165f63a7e31a7544f0
Paige Patton <prubenda@redhat.com>
2024-10-16 11:22:30 -0400

    adding arca folders
    Signed-off-by: Paige Patton <prubenda@redhat.com>

--- io-hog/prow_run.sh
+++ io-hog/prow_run.sh
@@ -9,7 +9,8 @@ source env.sh
 
 export KUBECONFIG=$KRKN_KUBE_CONFIG
 krkn_loc=/home/krkn/kraken
-SCENARIO_FOLDER="$krkn_loc/scenarios/kube/io-hog"
+sub_scenario_folder="scenarios/kube/io-hog"
+SCENARIO_FOLDER="$krkn_loc/$sub_scenario_folder"
 
 # cluster version
 echo "Printing cluster version"
@@ -23,12 +24,14 @@ setup_arcaflow_env "$SCENARIO_FOLDER"
 checks
 
 # Substitute config with environment vars defined
-export SCENARIO_FILE="scenarios/arcaflow/io-hog/input.yaml"
+export SCENARIO_FILE="$sub_scenario_folder/input.yaml"
 envsubst < config.yaml.template > $krkn_loc/io_hog_config.yaml
 
 # Run Kraken
-cat $krkn_loc/io_hog_config.yaml
-cat $SCENARIO_FOLDER/input.yaml
+
 cd $krkn_loc
+cat io_hog_config.yaml
+cat $SCENARIO_FILE
+
 python3.9 run_kraken.py --config=io_hog_config.yaml -o /tmp/report.out
 
--- memory-hog/prow_run.sh
+++ memory-hog/prow_run.sh
@@ -9,7 +9,8 @@ source env.sh
 
 export KUBECONFIG=$KRKN_KUBE_CONFIG
 krkn_loc=/home/krkn/kraken
-SCENARIO_FOLDER="$krkn_loc/scenarios/kube/memory-hog"
+sub_scenario_folder="scenarios/kube/memory-hog"
+SCENARIO_FOLDER="$krkn_loc/$sub_scenario_folder"
 
 # cluster version
 echo "Printing cluster version"
@@ -23,12 +24,13 @@ setup_arcaflow_env "$SCENARIO_FOLDER"
 checks
 
 # Substitute config with environment vars defined
-export SCENARIO_FILE="scenarios/arcaflow/memory-hog/input.yaml"
+export SCENARIO_FILE="$sub_scenario_folder/input.yaml"
 envsubst < config.yaml.template > $krkn_loc/memory_hog_config.yaml
 
 # Run Kraken
-cat $krkn_loc/memory_hog_config.yaml
-cat $SCENARIO_FOLDER/input.yaml
 cd $krkn_loc
+cat memory_hog_config.yaml
+cat $SCENARIO_FILE
+
 python3.9 run_kraken.py --config=memory_hog_config.yaml -o /tmp/report.out
 
